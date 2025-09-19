4f717cacc4e843a9ba63a2f79513ffda7f921fdd
Jose <jacumpianr22@gmail.com>
2025-09-19 08:09:37 -0500

    Base de datos y diagrama

---
+++ FormularioMulter/base_de_datos/musica.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE Musica 
+USE [Musica] 
+CREATE TABLE Artistas ( 
+    Id INT PRIMARY KEY IDENTITY(1,1), 
+    Nombre VARCHAR(100) NOT NULL, 
+    Nacionalidad VARCHAR(100), 
+    FechaNacimientos DATE 
+); 
+
+CREATE TABLE Albumes ( 
+    Id INT PRIMARY KEY IDENTITY(1,1), 
+    Titulo VARCHAR(200) NOT NULL, 
+    FechaLanzamiento DATE, 
+    IdArtista INT, 
+    FOREIGN KEY (IdArtista) REFERENCES Artistas(Id) 
+); 
+
+CREATE TABLE Canciones ( 
+    Id INT PRIMARY KEY IDENTITY(1,1), 
+    Titulo NVARCHAR(200) NOT NULL, 
+    Duracion INT, Genero VARCHAR(100), 
+    IdAlbum INT, IdArtista INT, 
+    FOREIGN KEY (IdAlbum) REFERENCES Albumes(Id), 
+    FOREIGN KEY (IdArtista) REFERENCES Artistas(Id) 
+);
+
+INSERT INTO Artistas (Nombre, Nacionalidad, FechaNacimientos)
+VALUES ('Shakira', 'Colombia', '1977-02-02');
+
+INSERT INTO Artistas (Nombre, Nacionalidad, FechaNacimientos)
+VALUES ('Coldplay', 'Reino Unido', '1996-01-01');
+
+INSERT INTO Artistas (Nombre, Nacionalidad, FechaNacimientos)
+VALUES ('The Beatles', 'Reino Unido', '1960-01-01');
+
+INSERT INTO Albumes (Titulo, FechaLanzamiento, IdArtista)
+VALUES ('Laundry Service', '2001-11-13', 1);
+
+INSERT INTO Albumes (Titulo, FechaLanzamiento, IdArtista)
+VALUES ('A Head Full of Dreams', '2015-12-04', 2);
+
+INSERT INTO Albumes (Titulo, FechaLanzamiento, IdArtista)
+VALUES ('Abbey Road', '1969-09-26', 3);
+
+-- Álbum de Shakira (IdAlbum = 1)
+INSERT INTO Canciones (Titulo, Duracion, Genero, IdAlbum, IdArtista)
+VALUES ('Whenever, Wherever', 210, 'Pop', 1, 1);
+
+-- Álbum de Coldplay (IdAlbum = 2)
+INSERT INTO Canciones (Titulo, Duracion, Genero, IdAlbum, IdArtista)
+VALUES ('Adventure of a Lifetime', 250, 'Pop Rock', 2, 2);
+
+-- Álbum de The Beatles (IdAlbum = 3)
+INSERT INTO Canciones (Titulo, Duracion, Genero, IdAlbum, IdArtista)
+VALUES ('Come Together', 259, 'Rock', 3, 3);
